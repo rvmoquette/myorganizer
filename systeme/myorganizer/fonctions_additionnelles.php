1e238e7e6b8e74d02db161bf8ccb44233ce1007d
herve.hautbois <herve.hautbois@gmail.com>
2020-02-22 15:47:03 +0100

    is connected

--- systeme/myorganizer/fonctions_additionnelles.php
+++ systeme/myorganizer/fonctions_additionnelles.php
@@ -8,3 +8,12 @@ function is_admin(): bool
 {
     return get_user_courant(MF_USER_ADMIN) === true;
 }
+
+/**
+ * Is user is connected
+ * @return bool
+ */
+function is_connected(): bool
+{
+    return get_user_courant(MF_USER__ID) !== null;
+}
